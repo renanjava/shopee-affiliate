ef78869dc41f4ab05eeba6f1397152d985539834
Renan <renancvr777@gmail.com>
2026-01-02 22:54:44 -0300

    build: adding force-dynamic

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { fetchProducts, getCategories } from '@/lib/fetchProducts';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     const products = await fetchProducts();
